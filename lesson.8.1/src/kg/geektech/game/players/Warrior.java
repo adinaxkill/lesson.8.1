724f72178d5c914aab604e646709d2e63b397811
adinaxkill <adinatimurova88@gmail.com>
2022-02-20 17:47:45 +0300

    Initial commit

---
+++ lesson.8.1/src/kg/geektech/game/players/Warrior.java
@@ -0,0 +1,14 @@
+package kg.geektech.game.players;
+
+import kg.geektech.game.general.RPG_Game;
+
+public class Warrior extends Hero {
+    public Warrior(int health, int damage) {
+        super(health, damage, SuperAbility.CRITICAL_DAMAGE);
+    }
+
+    @Override
+    public void applySuperPower(Boss boss, Hero[] heroes) {
+        int coeff = RPG_Game.random.nextInt(3) + 2;
+    }
+}
